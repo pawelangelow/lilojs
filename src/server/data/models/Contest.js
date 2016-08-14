913259da231cb45a1d596fa03fb069ffc5242248
Pavel Angelov <pawel.angelow@gmail.com>
2016-08-14 17:52:54 +0300

    Update contest model.

--- src/server/data/models/Contest.js
+++ src/server/data/models/Contest.js
@@ -7,12 +7,14 @@ var mongoose = require('mongoose'),
 module.exports.init = function () {
     var contestSchema = mongoose.Schema({
         title: { type: String, required: requiredMessage },
-        description: { type: String, required: requiredMessage },
+        description: { type: String },
         category: { type: String, required: requiredMessage },
         startDate: { type: Date, required: requiredMessage },
         endDate: { type: Date, required: requiredMessage },
-        creator: { type: Schema.Types.ObjectID, ref: 'User'},
-        problems: [{ type: Schema.Types.ObjectID, ref: 'Problem'}]
+        canPractice: { type: Boolean, required: requiredMessage, default: true },
+        isVisible: { type: Boolean, required: requiredMessage, default: false },
+        creator: { type: Schema.Types.ObjectID, ref: 'User' },
+        problems: [{ type: Schema.Types.ObjectID, ref: 'Problem' }]
     });
     mongoose.model('Contest', contestSchema);
 };
