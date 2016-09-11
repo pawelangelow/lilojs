145c1a5bd34d3c57437417eda4a853436988ed44
Pavel Angelov <pawel.angelow@gmail.com>
2016-09-11 18:30:11 +0300

    Edit submission to run C code problems.

--- src/server/services/running.js
+++ src/server/services/running.js
@@ -3,33 +3,90 @@
 /*jslint todo: true */
 'use strict';
 
-const async = require('async');
-
+var async = require('async');
+var fs = require('fs');
+var path = require('path');
 var exec = require('child_process').exec;
-var cmd = 'gcc sample-program.c -o program && ./program < input.txt';
-var cmdParameters = 'gcc sample-program2.c -o program && ./program 2 3';
 
 module.exports = {
     processSubmission: processSubmission
 };
 
 function processSubmission(code, tests, callbackResult) {
-    var tasks = [];
 
-      tasks.push(function (callback) {
-        exec(cmdParameters, function(error, stdout, stderr) {
-          if (!error) {
-            console.log(stdout);
-          } else {
-            console.log(error);
-            console.log("Sorry, no point for this test!");
-          }
-          callback();
+    var tasks = [],
+        pathToFolder = path.normalize(__dirname + '/../../server/resources/submissions'),
+        executableName = makeid(35),
+        programName = path.normalize(pathToFolder + '/' + executableName),
+        result = {tests: 0, successed: 0, failed: 0};
+
+    tasks.push(function (callback) {
+        fs.writeFile(programName + '.c', code, { flag: 'w' }, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            callback();
         });
-      });
-
-
-      async.series(tasks, function _allDone(err) {
-          callbackResult();
     });
+
+    tasks.push(function (callback) {
+        var cmdParameters = 'gcc ' + programName + '.c -o ' + programName;
+
+        exec(cmdParameters, function (error, stdout, stderr) {
+            if (!error) {
+                console.log(stdout);
+            } else {
+                console.log(error);
+            }
+            callback();
+        });
+    });
+
+    tests.forEach(function (test){
+        tasks.push(function (callback) {
+            var cmdParameters = 'cd ' + pathToFolder + ' && ./' + executableName + ' ' + test.input,
+                localResult,
+                i;
+
+            exec(cmdParameters, function (error, stdout, stderr) {
+                if (!error) {
+                    localResult = stdout.match(/[^\r\n]+/g);
+                    if (matchTest(test.output, localResult)) {
+                        result.successed++;
+                    } else {
+                        result.failed++;
+                    }
+                    result.tests++;
+                } else {
+                    console.log(error);
+                }
+                callback();
+            });
+        });
+    });
+
+    async.series(tasks, function _allDone(err) {
+        callbackResult(result);
+    });
+}
+
+function makeid(length) {
+    var text = "",
+        possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+        i;
+
+    for (i = 0; i < length; i += 1) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
+
+function matchTest(real, expected) {
+    for (var i = 0; i < expected.length; i++) {
+        if (expected[i] !== real[i]) {
+            return false;
+        }
+    }
+    return true;
 }
