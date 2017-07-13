699766717feef71b7b05c3c77dec661dba1c104c
Pavel Angelov <pawel.angelow@gmail.com>
2017-07-13 23:43:56 +0300

    Add administration UI.

---
+++ source/server/controllers/admin.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const router = require('express').Router();
+
+const pathResolver = require('../utilities').getViewName;
+
+module.exports = router;
+
+router.get('/', (req, res) => {
+	const viewName = pathResolver(__filename, ['index']);
+	res.render(viewName);
+});
+
+router.get('/addContest', (req, res) => {
+	const viewName = pathResolver(__filename, ['add-contest']);
+	res.render(viewName);
+});
+
+router.get('/addProblem', (req, res) => {
+	const viewName = pathResolver(__filename, ['add-problem']);
+	res.render(viewName);
+});
+
+router.get('/addTest', (req, res) => {
+	const viewName = pathResolver(__filename, ['add-test']);
+	res.render(viewName);
+});
