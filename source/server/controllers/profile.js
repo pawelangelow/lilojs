c29338aef4bf32370998ca49a42446e0815ccbb8
Pavel Angelov <pawel.angelow@gmail.com>
2017-07-22 10:58:50 +0300

    Add profile settings edit.

--- source/server/controllers/profile.js
+++ source/server/controllers/profile.js
@@ -2,11 +2,65 @@
 
 const router = require('express').Router();
 
-const pathResolver = require('../utilities').getViewName;
+const utils = require('../utilities');
+const pathResolver = utils.getViewName;
+const userService = require('../services/user');
+const onlyForLoggedIn = utils.onlyForLoggedIn;
+const pathPrefix = '/' + pathResolver(__filename, []);
 
 module.exports = router;
 
-router.get('/', (req, res) => {
+router.get('/', onlyForLoggedIn, (req, res) => {
 	const viewName = pathResolver(__filename, ['overview']);
-	res.render(viewName);
+
+	userService.getFullUserById(req.user._id).then((user) => {
+		res.render(viewName, {
+			user
+		});
+	});
+});
+
+router.post('/', onlyForLoggedIn, (req, res) => {
+	const currentUser = req.user;
+	const updatedData = req.body;
+	if (updatedData.password === '') {
+		delete updatedData.password;
+		delete updatedData.confirmPassword;
+	}
+
+	const viewName = pathResolver(__filename, ['overview']);
+
+	if (updatedData.password && updatedData.confirmPassword && updatedData.password !== updatedData.confirmPassword) {
+		const model = Object.assign({}, updatedData);
+
+		res.render(viewName, {
+			user: model,
+			errorMessage: 'Passwords do not match!',
+			start: 'credentials'
+		});
+	} else if (updatedData.password && updatedData.password.length <= 4) {
+		// TODO: Extract magic numbers to constants
+		const model = Object.assign({}, updatedData);
+
+		res.render(viewName, {
+			user: model,
+			errorMessage: 'Password should be more than 4 symbols!',
+			start: 'credentials'
+		});
+	} else {
+		userService
+			.updateData(updatedData, currentUser._id)
+			.then(() => {
+				req.session.success = 'Data was updated successfully!';
+				res.redirect(pathPrefix);
+			})
+			.catch((err) => {
+				err.unshift('There are errors!');
+				res.render(viewName, {
+					user: updatedData,
+					errorMessage: err.join('<br \>'),
+					start: 'credentials'
+				});
+			});
+	}
 });
