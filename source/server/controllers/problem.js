26e17fb60ad7c048812ea5c9c5efcef1cfdc04da
Pavel Angelov <pawel.angelow@gmail.com>
2017-09-09 19:55:09 +0300

    Add restriction for submitting solution.

--- source/server/controllers/problem.js
+++ source/server/controllers/problem.js
@@ -3,15 +3,17 @@
 const moment = require('moment');
 
 const router = require('express').Router();
-const pathResolver = require('../utilities').getViewName;
+const utils = require('../utilities');
+const pathResolver = utils.getViewName;
 const pathPrefix = '/' + pathResolver(__filename, []);
+const onlyForLoggedIn = utils.onlyForLoggedIn;
 
 const problems = require('../services/problem');
 const submissions = require('../services/submission');
 
 module.exports = router;
 
-router.get('/:id', (req, res) => {
+router.get('/:id', onlyForLoggedIn, (req, res) => {
 	const id = req.params.id;
 	const viewPath = pathResolver(__filename, ['index']);
 
@@ -30,7 +32,7 @@ router.get('/:id', (req, res) => {
 	});
 });
 
-router.post('/:id', (req, res) => {
+router.post('/:id', onlyForLoggedIn, (req, res) => {
 	const id = req.params.id;
 	const model = req.body;
 	model.sourceCode = model.code;
